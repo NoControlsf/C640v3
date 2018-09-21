47df3bd2feb2ec32882e567e48df959b6a674b90
Administrator <804937921@qq.com>
2018-09-21 14:25:31 +0800

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+import json
+from users.models import User
+# Create your views here.
+def index(request):
+    return render(request, 'basemain.html')
+
+def accounts_profile(request):
+    if request.method == 'POST':
+        a = json.loads(request.body)
+        print(a)
+        b = User.objects.get(email=request.user.email)
+        b.name = a['name']
+        b.save()
+    return render(request, 'accounts_profile.html')
